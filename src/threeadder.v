73f8e10ee3d8e757204de126f29c2a6f9d6c8136
nmcasorio <nmcasorio@gmail.com>
2024-06-20 10:04:07 -0500

    Create threeadder.v

---
+++ src/threeadder.v
@@ -0,0 +1,14 @@
+module tt_um_threeadder (input clk, input reset, input a, b, c, output x, y, z, f);
+
+    assign x =  ~a&~b&~c;
+
+    assign y =  ~a&b&c
+                a&b&~c 
+                a&~b&c;
+
+    assign z =  ~a&~b&c
+                ~a&b&~c
+                a&~b&~c
+
+    assign f =  a&b&c
+endmodule
